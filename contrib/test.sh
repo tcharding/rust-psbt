32840ada82213c32d890493320937e585c664877
Tobin C. Harding <me@tobin.cc>
2023-11-10 14:02:05 +1100

    Import psbt code from rust-bitcoin
    Import code from https://github.com/rust-bitcoin/rust-bitcoin` comit:
    `966b190f238408e24efd768ffa2bcdb6046e0cea Merge
    rust-bitcoin/rust-bitcoin#2168: Use network when calculating difficulty`
    
    Also grab required code from `bitcoin/src/serde_utils.rs` and
    `internals/src/error.rs`.
    
    Make changes to get things building:
    
    - Use the most terse public bitcoin API (ie, don't reach down into
      modules when importing if there is a re-export at crate level)
    - Fix paths to use correct `crate::` vs `bitcoin::`
    - Import `bitcoin::consensus::encode` as `consensus` and add TODO
      comments about it.

--- contrib/test.sh
+++ contrib/test.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-FEATURES="std"
+FEATURES="serde base64"
 MSRV="1\.63\.0"
 
 cargo --version
@@ -16,7 +16,15 @@ fi
 
 # Pin dependencies require to build with MSRV.
 if cargo --version | grep ${MSRV}; then
-    #
+    cargo update -p serde_json --precise 1.0.99
+    cargo update -p serde --precise 1.0.156
+    cargo update -p quote --precise 1.0.30
+    cargo update -p proc-macro2 --precise 1.0.63
+    cargo update -p serde_test --precise 1.0.175
+    # memchr 2.6.0 uses edition 2021
+    cargo update -p memchr --precise 2.5.0
+    # byteorder 1.5.0 uses edition 2021
+    cargo update -p byteorder --precise 1.4.3
 fi
 
 # Make all cargo invocations verbose.
@@ -31,13 +39,47 @@ then
     cargo clippy --locked --all-features --all-targets -- -D warnings
 fi
 
-# Test with no default features.
-cargo build --locked --no-default-features
-cargo test --locked --no-default-features
+echo "********* Testing std *************"
+# Test without any features other than std first
+cargo test --locked --verbose --no-default-features --features="std"
 
-# Test the std feature.
-cargo build --locked --no-default-features --features=std
-cargo test --locked --no-default-features --features=std
+echo "********* Testing default *************"
+# Then test with the default features
+cargo test --verbose
+
+if [ "$DO_NO_STD" = true ]
+then
+    echo "********* Testing no-std build *************"
+    # Build no_std, to make sure that cfg(test) doesn't hide any issues
+    cargo build --locked --verbose --features="no-std" --no-default-features
+
+    # Build std + no_std, to make sure they are not incompatible
+    cargo build --locked --verbose --features="no-std"
+
+    # Test no_std
+    cargo test --locked --verbose --features="no-std" --no-default-features
+
+    # Build all features
+    cargo build --locked --verbose --features="no-std $FEATURES" --no-default-features
+
+    # Build specific features
+    for feature in ${FEATURES}
+    do
+        cargo build --locked --verbose --features="no-std $feature" --no-default-features
+    done
+fi
+
+# Test each feature with default enabled ("std").
+for feature in ${FEATURES}
+do
+    echo "********* Testing $feature *************"
+    cargo test --locked --verbose --features="$feature"
+done
+
+# Build the docs if told to (this only works with the nightly toolchain)
+if [ "$DO_DOCSRS" = true ]; then
+    RUSTDOCFLAGS="--cfg docsrs -D warnings -D rustdoc::broken-intra-doc-links" cargo +nightly doc --all-features
+fi
 
 # Build the docs if told to (this only works with the nightly toolchain)
 if [ "$DO_DOCSRS" = true ]; then
---
+++ src/macros.rs
@@ -0,0 +1,164 @@
+// SPDX-License-Identifier: CC0-1.0
+
+#[allow(unused_macros)]
+macro_rules! combine {
+    ($thing:ident, $slf:ident, $other:ident) => {
+        if let (&None, Some($thing)) = (&$slf.$thing, $other.$thing) {
+            $slf.$thing = Some($thing);
+        }
+    };
+}
+
+macro_rules! impl_psbt_de_serialize {
+    ($thing:ty) => {
+        impl_psbt_serialize!($thing);
+        impl_psbt_deserialize!($thing);
+    };
+}
+
+macro_rules! impl_psbt_deserialize {
+    ($thing:ty) => {
+        impl $crate::serialize::Deserialize for $thing {
+            fn deserialize(bytes: &[u8]) -> Result<Self, $crate::Error> {
+                bitcoin::consensus::deserialize(&bytes[..]).map_err(|e| $crate::Error::from(e))
+            }
+        }
+    };
+}
+
+macro_rules! impl_psbt_serialize {
+    ($thing:ty) => {
+        impl $crate::serialize::Serialize for $thing {
+            fn serialize(&self) -> $crate::prelude::Vec<u8> { bitcoin::consensus::serialize(self) }
+        }
+    };
+}
+
+macro_rules! impl_psbtmap_serialize {
+    ($thing:ty) => {
+        impl $crate::serialize::Serialize for $thing {
+            fn serialize(&self) -> Vec<u8> { self.serialize_map() }
+        }
+    };
+}
+
+macro_rules! impl_psbtmap_deserialize {
+    ($thing:ty) => {
+        impl $crate::serialize::Deserialize for $thing {
+            fn deserialize(bytes: &[u8]) -> Result<Self, $crate::Error> {
+                let mut decoder = bytes;
+                Self::decode(&mut decoder)
+            }
+        }
+    };
+}
+
+macro_rules! impl_psbtmap_decoding {
+    ($thing:ty) => {
+        impl $thing {
+            pub(crate) fn decode<R: $crate::io::Read + ?Sized>(
+                r: &mut R,
+            ) -> Result<Self, $crate::Error> {
+                let mut rv: Self = core::default::Default::default();
+
+                loop {
+                    match $crate::raw::Pair::decode(r) {
+                        Ok(pair) => rv.insert_pair(pair)?,
+                        Err($crate::Error::NoMorePairs) => return Ok(rv),
+                        Err(e) => return Err(e),
+                    }
+                }
+            }
+        }
+    };
+}
+
+macro_rules! impl_psbtmap_ser_de_serialize {
+    ($thing:ty) => {
+        impl_psbtmap_decoding!($thing);
+        impl_psbtmap_serialize!($thing);
+        impl_psbtmap_deserialize!($thing);
+    };
+}
+
+#[rustfmt::skip]
+macro_rules! impl_psbt_insert_pair {
+    ($slf:ident.$unkeyed_name:ident <= <$raw_key:ident: _>|<$raw_value:ident: $unkeyed_value_type:ty>) => {
+        if $raw_key.key.is_empty() {
+            if $slf.$unkeyed_name.is_none() {
+                let val: $unkeyed_value_type = $crate::serialize::Deserialize::deserialize(&$raw_value)?;
+                $slf.$unkeyed_name = Some(val)
+            } else {
+                return Err($crate::Error::DuplicateKey($raw_key).into());
+            }
+        } else {
+            return Err($crate::Error::InvalidKey($raw_key).into());
+        }
+    };
+    ($slf:ident.$keyed_name:ident <= <$raw_key:ident: $keyed_key_type:ty>|<$raw_value:ident: $keyed_value_type:ty>) => {
+        if !$raw_key.key.is_empty() {
+            let key_val: $keyed_key_type = $crate::serialize::Deserialize::deserialize(&$raw_key.key)?;
+            match $slf.$keyed_name.entry(key_val) {
+                $crate::prelude::btree_map::Entry::Vacant(empty_key) => {
+                    let val: $keyed_value_type = $crate::serialize::Deserialize::deserialize(&$raw_value)?;
+                    empty_key.insert(val);
+                }
+                $crate::prelude::btree_map::Entry::Occupied(_) => return Err($crate::Error::DuplicateKey($raw_key).into()),
+            }
+        } else {
+            return Err($crate::Error::InvalidKey($raw_key).into());
+        }
+    };
+}
+
+#[rustfmt::skip]
+macro_rules! impl_psbt_get_pair {
+    ($rv:ident.push($slf:ident.$unkeyed_name:ident, $unkeyed_typeval:ident)) => {
+        if let Some(ref $unkeyed_name) = $slf.$unkeyed_name {
+            $rv.push($crate::raw::Pair {
+                key: $crate::raw::Key {
+                    type_value: $unkeyed_typeval,
+                    key: vec![],
+                },
+                value: $crate::serialize::Serialize::serialize($unkeyed_name),
+            });
+        }
+    };
+    ($rv:ident.push_map($slf:ident.$keyed_name:ident, $keyed_typeval:ident)) => {
+        for (key, val) in &$slf.$keyed_name {
+            $rv.push($crate::raw::Pair {
+                key: $crate::raw::Key {
+                    type_value: $keyed_typeval,
+                    key: $crate::serialize::Serialize::serialize(key),
+                },
+                value: $crate::serialize::Serialize::serialize(val),
+            });
+        }
+    };
+}
+
+// macros for serde of hashes
+macro_rules! impl_psbt_hash_de_serialize {
+    ($hash_type:ty) => {
+        impl_psbt_hash_serialize!($hash_type);
+        impl_psbt_hash_deserialize!($hash_type);
+    };
+}
+
+macro_rules! impl_psbt_hash_deserialize {
+    ($hash_type:ty) => {
+        impl $crate::serialize::Deserialize for $hash_type {
+            fn deserialize(bytes: &[u8]) -> Result<Self, $crate::Error> {
+                <$hash_type>::from_slice(&bytes[..]).map_err(|e| $crate::Error::from(e))
+            }
+        }
+    };
+}
+
+macro_rules! impl_psbt_hash_serialize {
+    ($hash_type:ty) => {
+        impl $crate::serialize::Serialize for $hash_type {
+            fn serialize(&self) -> $crate::prelude::Vec<u8> { self.as_byte_array().to_vec() }
+        }
+    };
+}
