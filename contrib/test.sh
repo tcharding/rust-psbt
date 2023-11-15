15996d2298072dd3e5edb42bad0b50087c63a14c
Tobin C. Harding <me@tobin.cc>
2023-11-15 13:08:40 +1100

    CI: Remove pinning
    We don't need these pin for the current MSRV, they came from 1.48.0 in
    `rust-bitcoin`.

--- contrib/test.sh
+++ contrib/test.sh
@@ -14,19 +14,6 @@ if cargo --version | grep nightly >/dev/null; then
     NIGHTLY=true
 fi
 
-# Pin dependencies require to build with MSRV.
-if cargo --version | grep ${MSRV}; then
-    cargo update -p serde_json --precise 1.0.99
-    cargo update -p serde --precise 1.0.156
-    cargo update -p quote --precise 1.0.30
-    cargo update -p proc-macro2 --precise 1.0.63
-    cargo update -p serde_test --precise 1.0.175
-    # memchr 2.6.0 uses edition 2021
-    cargo update -p memchr --precise 2.5.0
-    # byteorder 1.5.0 uses edition 2021
-    cargo update -p byteorder --precise 1.4.3
-fi
-
 # Make all cargo invocations verbose.
 export CARGO_TERM_VERBOSE=true
 
