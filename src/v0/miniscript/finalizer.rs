d4917d4a8c0c72fe66ba94525b2d00ab918d532a
Nick Johnson <nick@yonson.dev>
2026-04-29 13:45:29 -0700

    Remove //! spare line at end of headers
    Port of #2772 and #2798.

--- src/v0/miniscript/finalizer.rs
+++ src/v0/miniscript/finalizer.rs
@@ -6,7 +6,6 @@
 //! This module implements the Finalizer and Extractor roles defined in
 //! BIP 174, PSBT, described at
 //! `https://github.com/bitcoin/bips/blob/master/bip-0174.mediawiki`
-//!
 
 use core::convert::TryFrom;
 use core::mem;
