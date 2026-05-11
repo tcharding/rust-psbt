7402a653bf859643e0fef5377e86044b97715747
Nick Johnson <nick@yonson.dev>
2026-05-11 14:59:17 -0700

    Doc typos
    Backport of #5065

--- src/serialize.rs
+++ src/serialize.rs
@@ -527,7 +527,7 @@ mod tests {
 
     use super::*;
 
-    // Composes tree matching a given depth map, filled with dumb script leafs,
+    // Composes tree matching a given depth map, filled with dumb script leaves,
     // each of which consists of a single push-int op code, with int value
     // increased for each consecutive leaf.
     pub fn compose_taproot_builder<'map>(
